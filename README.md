ece935875eb0e65b2d6f80363a5b444aaae3c076
Vajean1 <nicolas53301@gmail.com>
2025-11-08 17:47:23 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# API do Projeto ServiçoJá (Back-end)
+
+Este é o repositório do back-end (API) do Projeto Integrador "ServiçoJá".
+
+**Links Rápidos:**
+* [Repositório Front-end](coloca)
+* [Quadro de Tarefas (ClickUp)](https://app.clickup.com/90132659754/v/o/s/901311434817)
+* [Documentação da API (Swagger)](http://127.0.0.1:8000/api/doc/) (só funciona com o servidor a rodar!!)
+
---
+++ config/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Rotas de Autenticação
+    # O front-end vai fazer POST aqui para obter o token
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Rotas do App "accounts"
+    #path('api/accounts/', include('accounts.urls')), 
+    
+    # Rotas de "login/logout" do DRF para teste.
+    path('api-auth/', include('rest_framework.urls')),
+
+    # --- ROTAS DA DOCUMENTAÇÃO (SWAGGER) ---
+
+    # A Rota 1 Gera o ficheiro "schema" da API
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # A Rota 2  A Interface Swagger UI
+    path('api/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
