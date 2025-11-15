f651765167c1cd7efc4cdeb8ba9724e87c4e3aaf
Vajean1 <nicolas53301@gmail.com>
2025-11-15 17:15:32 -0300

    Fixing bugs: status_service and service_review. Adding new filets

--- avaliacoes/admin.py
+++ avaliacoes/admin.py
@@ -3,7 +3,17 @@ from .models import Avaliacao
 
 @admin.register(Avaliacao)
 class AvaliacaoAdmin(admin.ModelAdmin):
-    list_display = ('cliente_user', 'prestador_user', 'nota', 'data_criacao')
-    list_filter = ('nota', 'data_criacao')
-    search_fields = ('cliente_user__email', 'prestador_user__email')
+    list_display = ('id', 'get_cliente', 'get_prestador', 'nota', 'data_criacao')
+    search_fields = (
+        'servico_realizado__cliente_user__email', 
+        'servico_realizado__prestador_user__email'
+    )
+    autocomplete_fields = ('servico_realizado',)
 
+    @admin.display(description='Cliente')
+    def get_cliente(self, obj):
+        return obj.servico_realizado.cliente_user
+
+    @admin.display(description='Prestador')
+    def get_prestador(self, obj):
+        return obj.servico_realizado.prestador_user
